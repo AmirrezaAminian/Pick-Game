23ae071634df0ceb88883d5209ace3eb3bcd827f
Amirreza Aminian <aminian.amirreza@gmail.com>
2022-07-18 15:49:24 +0430

    The game is almost done

--- script.js
+++ script.js
@@ -25,39 +25,77 @@ diceEl.classList.add('hidden') ;
 const scores = [0,0] ;
 let currentScore = 0 ;
 let activeplayer = 0 ;
+let playing = true ;
+
+
+const swichPlayer = function(){
+       document.getElementById(`current--${activeplayer}`).textContent = 0;
+       activeplayer = activeplayer === 0 ? 1 : 0;
+       currentScore = 0;
+
+       // toggle : aghe ke classi ke to parantez midim behesho dashte bashe hazf mishe , aghe nadashte bashe ezafe mishe
+
+       // ma aghe nagah koni dar vaghe class player active  hamoon backgroundas ke to avali be sorat otomat hastesh
+       player0El.classList.toggle('player--active');
+       player1El.classList.toggle('player--active');
+}
 
 
 // Rolling dice functionality 
 
 btnRoll.addEventListener('click' , function(){
-    // 1. Generating a random dice roll
-     const dice  = Math.trunc(Math.random() * 6) + 1 ;
 
-    // 2. Display dice
-    diceEl.classList.remove('hidden') ;
-    diceEl.src = `dice-${dice}.png`
+   if(playing) {
+     // 1. Generating a random dice roll
+     const dice = Math.trunc(Math.random() * 6) + 1;
 
-    // 3. Check for rolled 1 : if true , switch to next player
-    if(dice !== 1){
-        // Add dice to the current score
+     // 2. Display dice
+     diceEl.classList.remove('hidden');
+     diceEl.src = `dice-${dice}.png`;
 
-        // currentscore += dice ;
-        currentScore = currentScore + dice ;
-        document.getElementById(`current--${activeplayer}`).textContent  = currentScore ;
+     // 3. Check for rolled 1 : if true , switch to next player
+     if (dice !== 1) {
+       // Add dice to the current score
 
-        // current0El.textContent = currentScore ; // change 
+       // currentscore += dice ;
+       currentScore = currentScore + dice;
+       document.getElementById(`current--${activeplayer}`).textContent =
+         currentScore;
 
-    }else{
-        // Switch to the next player 
-         document.getElementById(`current--${activeplayer}`).textContent = 0;
-        activeplayer = activeplayer === 0 ? 1 : 0 ;
-        currentScore = 0
-        
-        // toggle : aghe ke classi ke to parantez midim behesho dashte bashe hazf mishe , aghe nadashte bashe ezafe mishe 
+       // current0El.textContent = currentScore ; // change
+     } else {
+       // Switch to the next player
+       swichPlayer();
+     }
+   }
+})
 
-        // ma aghe nagah koni dar vaghe class player active  hamoon backgroundas ke to avali be sorat otomat hastesh 
-        player0El.classList.toggle('player--active') ;
-        player1El.classList.toggle('player--active') ;
 
-    }
+btnHold.addEventListener('click' , function(){
+
+    if(playing){
+      // 1. Add current score to active plater's score
+      scores[activeplayer] += currentScore;
+      // scores[1] = scores[1] + currentscore
+
+      document.getElementById(`score--${activeplayer}`).textContent =
+        scores[activeplayer];
+
+      // 2. check if player's score is >= 100
+
+      if (scores[activeplayer] >= 10) {
+        // Finish the game
+        playing = false;
+        diceEl.classList.add('hidden');
+        document
+          .querySelector(`.player--${activeplayer}`)
+          .classList.add('player--winner');
+        document
+          .querySelector(`.player--${activeplayer}`)
+          .classList.remove('player--active');
+      } else {
+        // swich to next players
+        swichPlayer();
+      }
+    }    
 })
